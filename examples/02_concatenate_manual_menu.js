38b62939b0eb85f09945754c2c66b786b7271dd1
regorxxx <regorxxx@protonmail.com>
2022-03-04 18:48:25 +0100

    Code cleanup

--- examples/02_concatenate_manual_menu.js
+++ examples/02_concatenate_manual_menu.js
@@ -6,30 +6,30 @@ include('01_simple_menu.js'); // Uses the first menu and the next one as 'otherO
 
 var menuTwo = new _anotherMenu();
 function _anotherMenu() {
-    this.idxInitial = 0;
+	this.idxInitial = 0;
 	this.idxEnd = 0;
 
-    this.btn_up = (idxInitial) => {
-        this.idxInitial = idxInitial;
-        this.idxEnd = idxInitial;
-        const menuName = menu.getMainMenuName(); // here menu is the first menu created
-        menu.getMenu(menuName).AppendMenuItem(MF_STRING, ++this.idxEnd, 'Manual entry 1');
+	this.btn_up = (idxInitial) => {
+		this.idxInitial = idxInitial;
+		this.idxEnd = idxInitial;
+		const menuName = menu.getMainMenuName(); // here menu is the first menu created
+		menu.getMenu(menuName).AppendMenuItem(MF_STRING, ++this.idxEnd, 'Manual entry 1');
 		menu.getMenu(menuName).AppendMenuItem(MF_STRING, ++this.idxEnd, 'Manual entry 2');
-        return this.idxEnd;
-    };
-    
-    this.btn_up_done = (currIdx) => {
+		return this.idxEnd;
+	};
+	
+	this.btn_up_done = (currIdx) => {
 		this.idxEnd = this.idxInitial;
-        if (currIdx === ++this.idxEnd) {
-            console.log('Manual entry 1');
-            return;
-        }
-        if (currIdx === ++this.idxEnd) {
-            console.log('Manual entry 2');
-            return;
-        }
+		if (currIdx === ++this.idxEnd) {
+			console.log('Manual entry 1');
+			return;
+		}
+		if (currIdx === ++this.idxEnd) {
+			console.log('Manual entry 2');
+			return;
+		}
 		return;
-    };
+	};
 }
 
 var otherObj = menuTwo;
--- examples/04_concatenate_multiple_menus.js
+++ examples/04_concatenate_multiple_menus.js
@@ -14,7 +14,7 @@ menuFour.newEntry({entryText: 'Bye', func: () => {console.log('Bye');}});
 menuFour.newEntry({entryText: 'sep'});
 menuFour.newEntry({entryText: 'Bye 2', func: () => {console.log('Bye 2');}});
 
-// Note that mixing manual an entire framework objs has a limitation. Manual entries are always added at the end unless attached to a submenu
+// Note that mixing manual and entire framework objs has a limitation. Manual entries are always added at the end unless attached to a submenu
 // Because using AppendMenuItem directly on the menus, add them to the end of the list. But you can append items to an existing submenu instead.
 // i.e. otherObj = [menuTwo, menuThree, menuFour] is equivalent to [menuFour, menuTwo, menuThree] in any case, since menuTwo and menutThree, 
 // being manual menus are always called after the rest
